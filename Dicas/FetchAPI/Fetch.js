049b7316a8ef877fa198bbabc963ce86e6431c24
Rafael L. Campos <rafa.luck46@gmail.com>
2019-08-02 17:51:21 -0300

    Update logar dados
    Updated!!!!

--- Dicas/FetchAPI/Fetch.js
+++ Dicas/FetchAPI/Fetch.js
@@ -1,12 +1,21 @@
-let meuUsuario = 'ipetinate'
-let dados = []
 
-fetch('https://api.github.com/users/ipetinate')
-  .then(resposta => resposta.json())
-  .then(json => dados = json)
+(function(){
+    let meuUsuario = 'ipetinate'
+    let dados = []
 
-console.log(`Usuario: ${meuUsuario}`, dados)
+    fetch('https://api.github.com/users/ipetinate')
+      .then(resposta => {
+            return resposta.json();
+           })    
+      .then(json => {
+        dados.push(json);
+        console.log(dados);
+      })
 
-// fetch('./file.json')
-//   .then(response => response.json())
-//   .then(data => console.log(data))
+    console.log(`Usuario: ${meuUsuario}`, dados)
+
+    // fetch('./file.json')
+    //   .then(response => response.json())
+    //   .then(data => console.log(data))
+
+}())
